1698909b169f1caaf7edbbf6faf3edfd554f4333
howsillyistoosilly <howsillyistoosilly@gmail.com>
2026-02-17 09:54:02 +0530

    UC2-BruteForcePallindrome

--- PalindromeApp/src/UseCase1PalindromeCheckerApp.java
+++ PalindromeApp/src/UseCase1PalindromeCheckerApp.java
@@ -1,9 +1,27 @@
-public class UseCase1PalindromeCheckerApp{
-    public static void main (String[] args){
-        System.out.println("-----------------------------------------------------------");
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version 1.0");
-        System.out.println("System initialized successfully ");
-        System.out.println("-----------------------------------------------------------");
+import java.util.Scanner;
+
+public class UseCase1PalindromeCheckerApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String to Check for Palindrome:");
+        String word = sc.nextLine();
+
+        boolean isPalindrome = true;
+        int length = word.length();
+
+        for (int i = 0; i < length / 2; i++) {
+            if (word.charAt(i) != word.charAt(length - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is NOT a palindrome.");
+        }
+
+        sc.close();
     }
 }
